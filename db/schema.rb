0fac82b884baa8abd00c82795b1f729f82b34433
Dylan Conlin <dylan.conlin@gmail.com>
2012-04-04 12:47:16 -0700

    schema shit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120323194509) do
+ActiveRecord::Schema.define(:version => 20120327182747) do
+
+  create_table "departments", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "infractions", :force => true do |t|
     t.integer  "user_id"
