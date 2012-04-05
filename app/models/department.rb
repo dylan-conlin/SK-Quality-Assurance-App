19a7bb3eb971ee628052c244be56f61c2a27dd6c
Dylan Conlin <dylan.conlin@gmail.com>
2012-04-05 12:55:31 -0700

    optimized departments views

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,10 @@
 class Department < ActiveRecord::Base
-  has_many :users
+
+  attr_accessible :name
+
+  has_many  :users, :dependent => :destroy
+
+  validates :name, :presence => true,
+    :length => { :maximum => 50 }
+
 end
