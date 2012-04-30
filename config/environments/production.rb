014ba60118cefe09a2f5069f2ad1957864ab0ac0
Dylan Conlin <dylan.conlin@gmail.com>
2012-04-30 11:47:43 -0700

    added mail config to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,21 @@ SampleApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+# Change mail delvery to either :smtp, :sendmail, :file, :test
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "http://skfoodgroup.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: "dylan.conlin@gmail.com",
+  password: "eggboobsmashstink"
+}
+
+# Specify what domain to use for mailer URLs
+config.action_mailer.default_url_options = {host: "http://skfoodgroup.herokuapp.com")
+
+
 end
