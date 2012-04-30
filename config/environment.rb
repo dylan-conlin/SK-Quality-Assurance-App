d741e298e2a939dd5057d761a70d8f9b610fdf25
Dylan Conlin <dylan.conlin@gmail.com>
2012-04-30 11:57:06 -0700

    added email config info to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,22 @@ require File.expand_path('../application', __FILE__)
 SampleApp::Application.initialize!
 
 Date::DATE_FORMATS.merge!(:default => "%m/%d/%Y")
+
+config.action_mailer.delivery_method = :smtp
+
+
+
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "http://skfoodgroup.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: "dylan.conlin@gmail.com",
+  password: "eggboobsmashstink"
+}
+
+# Specify what domain to use for mailer URLs
+config.action_mailer.default_url_options = {host: "skfoodgroup.herokuapp.com"}
+
+config.action_mailer.raise_delivery_errors = true
