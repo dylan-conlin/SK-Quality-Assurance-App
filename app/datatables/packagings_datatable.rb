80aab86a5b21b461320aa261c2d91ef3678dd9d4
Dylan Conlin <dylan.conlin@gmail.com>
2012-05-17 14:40:16 -0700

    released user name on index

--- app/datatables/packagings_datatable.rb
+++ app/datatables/packagings_datatable.rb
@@ -22,8 +22,8 @@ private
     packagings.map do |packaging|
       [
         h(packaging.created_at),
-        gravatar_for(packaging.user)
-        # link_to(packaging.user.name, packaging.user),
+        gravatar_for(packaging.user),
+        link_to(packaging.user.name, packaging.user),
         # h(packaging.workorder),
         # h(packaging.item_number),
         # h(packaging.individual_label_placement),
