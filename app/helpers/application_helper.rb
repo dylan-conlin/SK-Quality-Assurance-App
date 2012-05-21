b29a6500d39daf9cf8bd6db92b02b4188af4304d
Dylan Conlin <dylan.conlin@gmail.com>
2012-05-21 15:12:42 -0700

    changed base title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ def sortable(column, title = nil)
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Dylan's webapp"
+    base_title = "SK Food Group"
     if page_title.empty?
       base_title
     else
