e9d3ec4ea69a38e63f18a7dc62ad8481bc6fef4e
exit <dylan.conlin@gmail.com>
2012-06-06 16:17:03 -0700

    fixed comments stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120604225854) do
+ActiveRecord::Schema.define(:version => 20120606231310) do
 
   create_table "celebrities", :force => true do |t|
     t.string   "name"
