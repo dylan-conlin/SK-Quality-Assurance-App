84aed9488ffceb7c5ce37739e9e2a645d6635724
exit <dylan.conlin@gmail.com>
2012-06-13 11:07:14 -0700

    increased character length on infractions description

--- app/models/infraction.rb
+++ app/models/infraction.rb
@@ -2,7 +2,7 @@ class Infraction < ActiveRecord::Base
   belongs_to :user
 
   validates :user_id,  presence: true, length: { maximum: 50 }
-  validates :description,  presence: true, length: { maximum: 280 }
+  validates :description,  presence: true, length: { maximum: 500 }
   validates :date,  presence: true
 
 end
