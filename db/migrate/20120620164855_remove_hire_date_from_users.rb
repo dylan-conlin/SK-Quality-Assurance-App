18d05c4ecf6d4f130d6a8f49dbeccaa36d01c085
exit <dylan.conlin@gmail.com>
2012-06-20 11:23:06 -0700

    removed remove hire date migration

--- db/migrate/20120620164855_remove_hire_date_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveHireDateFromUsers < ActiveRecord::Migration
-  def up
-    remove_column :users, :hire_date
-      end
-
-  def down
-    add_column :users, :hire_date, :string
-  end
-end
