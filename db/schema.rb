0f26b77451718674535c4babda7d394cb661f45d
exit <dylan.conlin@gmail.com>
2012-07-26 09:20:32 -0700

    changes..

--- db/schema.rb
+++ db/schema.rb
@@ -79,6 +79,13 @@ ActiveRecord::Schema.define(:version => 20120725193442) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "documents", :force => true do |t|
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "foreign_objects", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
@@ -144,6 +151,7 @@ ActiveRecord::Schema.define(:version => 20120725193442) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.decimal  "score"
   end
 
   create_table "items", :force => true do |t|
@@ -175,7 +183,6 @@ ActiveRecord::Schema.define(:version => 20120725193442) do
   create_table "packagings", :force => true do |t|
     t.integer  "user_id"
     t.string   "workorder"
-    t.string   "item_number"
     t.boolean  "individual_label_placement"
     t.boolean  "individual_label_legibility"
     t.boolean  "individual_seal_integrity"
@@ -244,5 +251,31 @@ ActiveRecord::Schema.define(:version => 20120725193442) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+
+  create_table "users_table", :id => false, :force => true do |t|
+    t.integer "id"
+    t.text    "name"
+  end
+
+  create_table "versions", :force => true do |t|
+    t.integer  "versioned_id"
+    t.string   "versioned_type"
+    t.integer  "user_id"
+    t.string   "user_type"
+    t.string   "user_name"
+    t.text     "modifications"
+    t.integer  "number"
+    t.integer  "reverted_from"
+    t.string   "tag"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  add_index "versions", ["created_at"], :name => "index_versions_on_created_at"
+  add_index "versions", ["number"], :name => "index_versions_on_number"
+  add_index "versions", ["tag"], :name => "index_versions_on_tag"
+  add_index "versions", ["user_id", "user_type"], :name => "index_versions_on_user_id_and_user_type"
+  add_index "versions", ["user_name"], :name => "index_versions_on_user_name"
+  add_index "versions", ["versioned_id", "versioned_type"], :name => "index_versions_on_versioned_id_and_versioned_type"
 
 end
