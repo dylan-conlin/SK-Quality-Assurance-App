bcc39aa9647ad0dabd3a1be392eefca97174abff
exit <dylan.conlin@gmail.com>
2012-08-02 08:50:26 -0700

    disabled new user confirm email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      UserMailer.signup_confirmation(@user).deliver
+     # UserMailer.signup_confirmation(@user).deliver
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
