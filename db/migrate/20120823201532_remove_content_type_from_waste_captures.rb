aec6bff3ec68b9108e672f4ddfc2752e0339115c
Dylan Conlin <dylan.conlin@gmail.com>
2012-08-23 14:35:26 -0700

    removed content type migraton

--- db/migrate/20120823201532_remove_content_type_from_waste_captures.rb
+++
@@ -1,9 +0,0 @@
-class RemoveContentTypeFromWasteCaptures < ActiveRecord::Migration
-  def up
-    remove_column :waste_captures, :content_type
-      end
-
-  def down
-    add_column :waste_captures, :content_type, :string
-  end
-end
