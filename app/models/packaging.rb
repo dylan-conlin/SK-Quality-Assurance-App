344237dee5a55faef6baacb92f486ce937f64a28
exit <dylan.conlin@gmail.com>
2012-08-24 09:27:05 -0700

    fixed packaging

--- app/models/packaging.rb
+++ app/models/packaging.rb
@@ -1,9 +1,9 @@
 class Packaging < ActiveRecord::Base
-  # attr_accessible :user_id, :workorder, :item_number, :individual_label_placement, :individual_label_legibility, :individual_seal_integrity, :individual_label_accuracy, :individual_general_appearance, :master_label_placement, :master_label_legibility, :master_tape_glue, :master_case_appearance, :case_count, :case_weight, :comments
+ attr_accessible :user_id, :workorder, :individual_label_placement, :individual_label_legibility, :individual_seal_integrity, :individual_label_accuracy, :individual_general_appearance, :master_label_placement, :master_label_legibility, :master_tape_glue, :master_case_appearance, :case_count, :case_weight, :comments, :item_id
 
  belongs_to :user
  belongs_to :item
- validates :user_id, :item_id, :workorder, :case_count, :case_weight, :presence => true
+ validates :user_id, :workorder, :case_count, :case_weight, :presence => true
 
  validates_numericality_of :case_count, :greater_than_or_equal_to => 0
  validates_numericality_of :case_weight, :greater_than_or_equal_to => 0
